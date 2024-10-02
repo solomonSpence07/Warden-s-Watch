d69e2134f96dadc37de607ca7c6959a42f5e58ae
solomonSpence07 <sspence2@ftstudent.org>
2024-10-02 12:47:57 -0400

    fixed 1 issue

--- Scripts/wave_control.gd
+++ Scripts/wave_control.gd
@@ -6,8 +6,8 @@ var wave_duration = 5.0  # Time before the next wave starts
 var enemy_a= preload("res://scenes/enemy_a.tscn")
 var enemy_b= preload("res://scenes/enemy_b.tscn")
 var enemy_c= preload("res://scenes/enemy_c.tscn")
-@onready var path = $Path2D
-@onready var wave_timer = $waveTimer
+@onready var path = %Path2D
+@onready var wave_timer: Timer = $waveTimer
 
 func _ready():
 	start_next_wave()
@@ -20,7 +20,8 @@ func spawn_enemies(enemy_count:int,type:PackedScene):
 	for i in range(enemy_count):
 		var enemy = type.instantiate()
 		path.add_child(enemy)
-		
+		wave_timer.start()
+		await wave_timer.timeout
 
 	# Use await with a Timer instance
 	var timer = Timer.new()
