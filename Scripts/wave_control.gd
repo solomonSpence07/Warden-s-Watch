6bab4b1cb798c06ff7eff8667cad08442e0ad83b
solomonSpence07 <sspence2@ftstudent.org>
2024-10-04 12:01:12 -0400

    some stuff

--- Scripts/wave_control.gd
+++ Scripts/wave_control.gd
@@ -1,42 +1,38 @@
-extends Timer
+extends Label
 
 @export var current_wave_number = 0
-var wave_duration = 20  # Time before the next wave starts
 var enemy_a= preload("res://scenes/enemy_a.tscn")
 var enemy_b= preload("res://scenes/enemy_b.tscn")
 var enemy_c= preload("res://scenes/enemy_c.tscn")
 @onready var path = %Path2D
 @onready var gapTimer: Timer= $gapTimer
 func _ready():
-	gapTimer.wait_time=wave_duration
-	start_next_wave()
+	
+	pass
 
 func start_next_wave():
+	print(current_wave_number)
 	current_wave_number += 1
 	if current_wave_number<=1:
-		await spawn_enemies(10,enemy_b) 
-		end_wave()
+		spawn_enemies(10,enemy_b) 
+		
 	elif current_wave_number<=5:
-		await spawn_enemies(current_wave_number*10,enemy_b)
-		end_wave()
+		spawn_enemies(current_wave_number*5,enemy_b)
+	
 	elif current_wave_number<=10:
 		await spawn_enemies(10,enemy_b)
 		await spawn_enemies(5,enemy_a)
-		await spawn_enemies(10,enemy_b)
-		end_wave()
+		spawn_enemies(10,enemy_b)
 
 func spawn_enemies(enemy_count:int,type:PackedScene):
 	for i in range(enemy_count):
 		var enemy = type.instantiate()
 		path.add_child(enemy)
-		self.start()
-		await self.timeout
-
+		gapTimer.start()
+		await gapTimer.timeout
+	print("spawn")
 	
-
-func end_wave():
-	print("Wave ", current_wave_number, " completed!")
-	# Use await with a Timer instance
-	gapTimer.start()
-	await gapTimer.timeout  # Wait for the timer to timeout
-	start_next_wave()
+func _process(delta):
+	if Input.is_action_just_pressed("waveStart"):
+		start_next_wave()
+		print("button clicked")
